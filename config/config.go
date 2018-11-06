eb64f47f2c60827f947eb7aac20ea5555fbd2ffb
jenson <jenson.c@go-jek.com>
2018-11-06 14:04:40 +0530

    [Amir/Jenson] Creating a method to load proctor configs in non-singleton manner

--- config/config.go
+++ config/config.go
@@ -7,6 +7,49 @@ import (
 	"github.com/spf13/viper"
 )
 
+const (
+	Environment = "ENVIRONMENT"
+	Host        = "PROCTOR_HOST"
+	Email       = "EMAIL_ID"
+	Token       = "ACCESS_TOKEN"
+)
+
+type ProctorConfig struct {
+	Host        string
+	Email       string
+	AccessToken string
+}
+
+func LoadConfig() (ProctorConfig, error) {
+	viper.AutomaticEnv()
+
+	viper.AddConfigPath(ConfigFileDir())
+	viper.SetConfigName("proctor")
+	viper.SetConfigType("yaml")
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		return ProctorConfig{}, err
+	}
+
+	proctorHost := viper.GetString(Host)
+	emailId := viper.GetString(Email)
+	accessToken := viper.GetString(Token)
+	return ProctorConfig{Host: proctorHost, Email: emailId, AccessToken: accessToken}, nil
+}
+
+// Returns Config file directory
+// Returns /tmp if environment variable `ENVIRONMENT` is set to test, otherwise returns $HOME/.proctor
+// This allows to test on dev environment without conflicting with installed proctor config file
+func ConfigFileDir() string {
+	if os.Getenv("ENVIRONMENT") == "test" {
+		return "/tmp"
+	} else {
+		return fmt.Sprintf("%s/.proctor", os.Getenv("HOME"))
+	}
+}
+
 func InitConfig() {
 	viper.SetConfigType("yaml")
 	viper.AutomaticEnv()
--- config/config_test.go
+++ config/config_test.go
@@ -1,6 +1,7 @@
 package config_test
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -8,6 +9,64 @@ import (
 	"github.com/gojektech/proctor/config"
 	"github.com/stretchr/testify/assert"
 )
+
+func setUp() {
+	os.Setenv(config.Environment, "test")
+}
+
+func TestReturnsConfigDirAsTmpIfEnvironmentIsTest(t *testing.T) {
+	os.Setenv(config.Environment, "test")
+	dir := config.ConfigFileDir()
+	assert.Equal(t, "/tmp", dir)
+}
+
+func TestReturnsConfigDirAsHomeDotProctorIfEnvironmentIsNotSet(t *testing.T) {
+	os.Unsetenv(config.Environment)
+
+	dir := config.ConfigFileDir()
+	expectedDir := fmt.Sprintf("%s/.proctor", os.Getenv("HOME"))
+	assert.Equal(t, expectedDir, dir)
+}
+
+func TestLoadConfigsFromEnvironmentVariables(t *testing.T) {
+	setUp()
+	proctorHost := "test.example.com"
+	email := "user@example.com"
+	accessToken := "test-token"
+	os.Setenv(config.Host, proctorHost)
+	os.Setenv(config.Email, email)
+	os.Setenv(config.Token, accessToken)
+	configFilePath := createProctorConfigFile(t, "")
+	defer os.Remove(configFilePath)
+
+	proctorConfig, err := config.LoadConfig()
+
+	assert.NoError(t, err)
+	assert.Equal(t, config.ProctorConfig{Host: proctorHost, Email: email, AccessToken: accessToken}, proctorConfig)
+}
+
+func TestLoadConfigFromFile(t *testing.T) {
+	setUp()
+	os.Unsetenv(config.Host)
+	os.Unsetenv(config.Email)
+	os.Unsetenv(config.Token)
+
+	configFilePath := createProctorConfigFile(t, "PROCTOR_HOST: file.example.com\nEMAIL_ID: file@example.com\nACCESS_TOKEN: file-token")
+	defer os.Remove(configFilePath)
+
+	proctorConfig, err := config.LoadConfig()
+
+	assert.NoError(t, err)
+	assert.Equal(t, config.ProctorConfig{Host: "file.example.com", Email: "file@example.com", AccessToken: "file-token"}, proctorConfig)
+}
+
+func createProctorConfigFile(t *testing.T, content string) string {
+	proctorHost := []byte(fmt.Sprintf(content))
+	configFilePath := fmt.Sprintf("%s/proctor.yaml", config.ConfigFileDir())
+	err := ioutil.WriteFile(configFilePath, proctorHost, 0644)
+	assert.NoError(t, err)
+	return configFilePath
+}
 
 func TestProctorHost(t *testing.T) {
 	proctorConfigFilePath := "/tmp/proctor.yaml"
