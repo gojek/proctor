136fca0cf6a8250fb44ddd90c0a7aec3be555ac3
Aishwarya Kaneri <aishwarya.kaneri@gmail.com>
2019-02-08 18:01:23 +0530

    add callback URL support for job execution API

--- proctord/jobs/execution/job.go
+++ proctord/jobs/execution/job.go
@@ -1,6 +1,8 @@
 package execution
 
 type Job struct {
-	Name string            `json:"name"`
-	Args map[string]string `json:"args"`
+	Name        string            `json:"name"`
+	Args        map[string]string `json:"args"`
+	CallbackApi string            `json:"callback_api"`
 }
+
