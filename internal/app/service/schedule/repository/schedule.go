8dc4c3b178e84af7129770b52343d566cb86947e
Deny Prasetyo <jasoet87@gmail.com>
2019-07-11 13:57:18 +0530

    [Jasoet|Bimozx] V2: Complete Schedule Repository Test

--- internal/app/service/schedule/repository/schedule.go
+++ internal/app/service/schedule/repository/schedule.go
@@ -5,13 +5,17 @@ import (
 	"proctor/internal/app/service/infra/db/postgresql"
 	"proctor/internal/app/service/infra/id"
 	"proctor/internal/app/service/schedule/model"
+	"time"
 )
 
 type ScheduleRepository interface {
 	Insert(context *model.Schedule) (uint64, error)
 	Delete(id uint64) error
 	GetById(id uint64) (*model.Schedule, error)
+	Disable(id uint64) error
+	Enable(id uint64) error
 	GetByUserEmail(userEmail string) ([]model.Schedule, error)
+	GetByJobName(jobName string) ([]model.Schedule, error)
 	GetAllEnabled() ([]model.Schedule, error)
 	GetAll() ([]model.Schedule, error)
 	GetEnabledById(id uint64) (*model.Schedule, error)
@@ -26,6 +30,28 @@ func NewScheduleRepository(client postgresql.Client) ScheduleRepository {
 	return &scheduleRepository{
 		postgresqlClient: client,
 	}
+}
+
+func (repository *scheduleRepository) Enable(id uint64) error {
+	sql := "UPDATE schedule SET enabled = :enabled, updated_at = :updated_at WHERE id = :id"
+	context := model.Schedule{
+		ID:        id,
+		UpdatedAt: time.Now(),
+		Enabled:   true,
+	}
+	_, err := repository.postgresqlClient.NamedExec(sql, &context)
+	return err
+}
+
+func (repository *scheduleRepository) Disable(id uint64) error {
+	sql := "UPDATE schedule SET enabled = :enabled, updated_at = :updated_at WHERE id = :id"
+	context := model.Schedule{
+		ID:        id,
+		UpdatedAt: time.Now(),
+		Enabled:   false,
+	}
+	_, err := repository.postgresqlClient.NamedExec(sql, &context)
+	return err
 }
 
 func (repository *scheduleRepository) Insert(context *model.Schedule) (uint64, error) {
@@ -74,6 +100,17 @@ func (repository *scheduleRepository) GetByUserEmail(userEmail string) ([]model.
 	return schedules, nil
 }
 
+func (repository *scheduleRepository) GetByJobName(jobName string) ([]model.Schedule, error) {
+	sql := "SELECT id, job_name, args, cron, notification_emails, user_email, \"group\", enabled, created_at, updated_at FROM schedule WHERE job_name=$1 "
+	var schedules []model.Schedule
+	err := repository.postgresqlClient.Select(&schedules, sql, jobName)
+	if err != nil {
+		return nil, err
+	}
+
+	return schedules, nil
+}
+
 func (repository *scheduleRepository) GetAllEnabled() ([]model.Schedule, error) {
 	sql := "SELECT id, job_name, args, cron, notification_emails, user_email, \"group\", enabled, created_at, updated_at FROM schedule WHERE enabled=$1 "
 	var schedules []model.Schedule
@@ -102,6 +139,10 @@ func (repository *scheduleRepository) GetEnabledById(id uint64) (*model.Schedule
 	err := repository.postgresqlClient.Select(&schedules, sql, true, id)
 	if err != nil {
 		return nil, err
+	}
+
+	if len(schedules) == 0 {
+		return nil, errors.Errorf("Execution context with id %v is not found!", id)
 	}
 
 	return &schedules[0], nil
--- internal/app/service/schedule/repository/schedule_test.go
+++ internal/app/service/schedule/repository/schedule_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/suite"
 	"proctor/internal/app/service/infra/db/postgresql"
 	"proctor/internal/app/service/schedule/model"
+	"strconv"
 	"testing"
 )
 
@@ -86,6 +87,202 @@ func (suite *ScheduleTestSuite) TestScheduleRepository_Delete() {
 	expectedSchedule, err := suite.repository.GetById(id)
 	assert.Error(t, err)
 	assert.Nil(t, expectedSchedule)
+}
+
+func (suite *ScheduleTestSuite) TestScheduleRepository_GetAll() {
+	t := suite.T()
+	recordCount := 15
+	err := populateSeedDataForTest(suite.repository, recordCount, map[string]string{})
+	assert.NoError(t, err)
+
+	schedules, err := suite.repository.GetAll()
+	assert.NoError(t, err)
+	assert.NotNil(t, schedules)
+	size := len(schedules)
+	assert.Equal(t, recordCount, size)
+}
+
+func (suite *ScheduleTestSuite) TestScheduleRepository_GetByUserEmail() {
+	t := suite.T()
+	recordCount := 15
+	err := populateSeedDataForTest(suite.repository, recordCount, map[string]string{})
+	assert.NoError(t, err)
+
+	suppliedEmail := "bimo.horizon@gojek.co.id"
+	withEmailCount := 14
+	err = populateSeedDataForTest(suite.repository, withEmailCount, map[string]string{"UserEmail": suppliedEmail})
+
+	schedules, err := suite.repository.GetByUserEmail(suppliedEmail)
+	assert.NoError(t, err)
+	assert.NotNil(t, schedules)
+	size := len(schedules)
+	assert.Equal(t, withEmailCount, size)
+
+	for _, schedule := range schedules {
+		assert.Equal(t, suppliedEmail, schedule.UserEmail)
+	}
+}
+
+func (suite *ScheduleTestSuite) TestScheduleRepository_GetByJobName() {
+	t := suite.T()
+	recordCount := 15
+	err := populateSeedDataForTest(suite.repository, recordCount, map[string]string{})
+	assert.NoError(t, err)
+
+	suppliedJobName := "bimo-awesome-job"
+	withJobName := 14
+	err = populateSeedDataForTest(suite.repository, withJobName, map[string]string{"JobName": suppliedJobName})
+
+	schedules, err := suite.repository.GetByJobName(suppliedJobName)
+	assert.NoError(t, err)
+	assert.NotNil(t, schedules)
+	size := len(schedules)
+	assert.Equal(t, withJobName, size)
+
+	for _, schedule := range schedules {
+		assert.Equal(t, suppliedJobName, schedule.JobName)
+	}
+}
+
+func (suite *ScheduleTestSuite) TestScheduleRepository_GetAllEnabled() {
+	t := suite.T()
+	recordCount := 15
+	err := populateSeedDataForTest(suite.repository, recordCount, map[string]string{"Enabled": "false"})
+	assert.NoError(t, err)
+
+	withJobName := 14
+	err = populateSeedDataForTest(suite.repository, withJobName, map[string]string{"Enabled": "true"})
+
+	schedules, err := suite.repository.GetAllEnabled()
+	assert.NoError(t, err)
+	assert.NotNil(t, schedules)
+	size := len(schedules)
+	assert.Equal(t, withJobName, size)
+
+	for _, schedule := range schedules {
+		assert.True(t, schedule.Enabled)
+	}
+}
+
+func (suite *ScheduleTestSuite) TestScheduleRepository_GetEnabledById() {
+	t := suite.T()
+	recordCount := 15
+	err := populateSeedDataForTest(suite.repository, recordCount, map[string]string{})
+	assert.NoError(t, err)
+
+	mapKey := fake.FirstName()
+	mapValue := fake.LastName()
+	schedule := &model.Schedule{
+		JobName:   fake.BuzzWord(),
+		UserEmail: fake.Email(),
+		Args: map[string]string{
+			mapKey: mapValue,
+		},
+		Cron:               "5 * * * *",
+		Tags:               fake.BeerMalt(),
+		NotificationEmails: fake.Email(),
+		Group:              fake.HackerIngverb(),
+		Enabled:            true,
+	}
+
+	id, err := suite.repository.Insert(schedule)
+	assert.NotNil(t, id)
+	assert.NoError(t, err)
+
+	expectedSchedule, err := suite.repository.GetEnabledById(id)
+	assert.NoError(t, err)
+	assert.NotNil(t, expectedSchedule)
+	assert.True(t, expectedSchedule.Enabled)
+
+	willNotExistsId := uint64(17777717)
+	unexpectedSchedule, err := suite.repository.GetEnabledById(willNotExistsId)
+	assert.Error(t, err)
+	assert.Nil(t, unexpectedSchedule)
+
+}
+
+func (suite *ScheduleTestSuite) TestScheduleRepository_EnableDisable() {
+	t := suite.T()
+	mapKey := fake.FirstName()
+	mapValue := fake.LastName()
+	schedule := &model.Schedule{
+		JobName:   fake.BuzzWord(),
+		UserEmail: fake.Email(),
+		Args: map[string]string{
+			mapKey: mapValue,
+		},
+		Cron:               "5 * * * *",
+		Tags:               fake.BeerMalt(),
+		NotificationEmails: fake.Email(),
+		Group:              fake.HackerIngverb(),
+		Enabled:            true,
+	}
+
+	id, err := suite.repository.Insert(schedule)
+	assert.NotNil(t, id)
+	assert.NoError(t, err)
+
+	expectedSchedule, err := suite.repository.GetById(id)
+	assert.NoError(t, err)
+	assert.NotNil(t, expectedSchedule)
+	assert.True(t, expectedSchedule.Enabled)
+
+	err = suite.repository.Disable(id)
+	assert.NoError(t, err)
+
+	expectedSchedule, err = suite.repository.GetById(id)
+	assert.NoError(t, err)
+	assert.NotNil(t, expectedSchedule)
+	assert.False(t, expectedSchedule.Enabled)
+
+	err = suite.repository.Enable(id)
+	assert.NoError(t, err)
+
+	expectedSchedule, err = suite.repository.GetById(id)
+	assert.NoError(t, err)
+	assert.NotNil(t, expectedSchedule)
+	assert.True(t, expectedSchedule.Enabled)
+
+}
+
+func populateSeedDataForTest(repository ScheduleRepository, count int, seedField map[string]string) error {
+	for i := 0; i < count; i++ {
+		fake.Seed(0)
+		var jobName = fake.BuzzWord()
+		if val, ok := seedField["JobName"]; ok {
+			jobName = val
+		}
+
+		var email = fake.Email()
+		if val, ok := seedField["UserEmail"]; ok {
+			email = val
+		}
+
+		var enabled = fake.Bool()
+		if val, ok := seedField["Enabled"]; ok {
+			enabled, _ = strconv.ParseBool(val)
+		}
+
+		schedule := &model.Schedule{
+			JobName:   jobName,
+			UserEmail: email,
+			Args: map[string]string{
+				fake.FirstName(): fake.LastName(),
+			},
+			Cron:               "5 * * * *",
+			Tags:               fake.BeerMalt(),
+			NotificationEmails: fake.Email(),
+			Group:              fake.HackerIngverb(),
+			Enabled:            enabled,
+		}
+
+		_, err := repository.Insert(schedule)
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (suite *ScheduleTestSuite) TearDownSuite() {
