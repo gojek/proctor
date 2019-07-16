dc6fbf8baa2c9a6108104a4e7d82ebaf89b5e43e
Bimo Horizon <bimo.horizon@go-jek.com>
2019-07-16 14:38:52 +0530

    [bimo.horizon|jasoet] Fix insufficient mock

--- internal/app/service/execution/service/execution_mock.go
+++ internal/app/service/execution/service/execution_mock.go
@@ -22,8 +22,9 @@ func (mockService *MockExecutionService) ExecuteWithCommand(jobName string, user
 	return arguments.Get(0).(*model.ExecutionContext), arguments.String(1), arguments.Error(2)
 }
 
-func (mockService *MockExecutionService) save(executionContext *model.ExecutionContext) {
-	mockService.Called(executionContext)
+func (mockService *MockExecutionService) save(executionContext *model.ExecutionContext) error {
+	args := mockService.Called(executionContext)
+	return args.Error(0)
 }
 
 func (mockService *MockExecutionService) StreamJobLogs(executionName string, waitTime time.Duration) (io.ReadCloser, error) {
