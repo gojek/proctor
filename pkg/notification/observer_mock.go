c7767a88faffce008d6f5e885b3e3b56ea7945d4
William Albertus Dembo <william.dembo@go-jek.com>
2019-09-17 14:51:08 +0700

    [Dembo] Add observer mock

---
+++ pkg/notification/observer_mock.go
@@ -0,0 +1,15 @@
+package notification
+
+import (
+	"github.com/stretchr/testify/mock"
+	"proctor/pkg/notification/event"
+)
+
+type ObserverMock struct {
+	mock.Mock
+}
+
+func (m *ObserverMock) OnNotify(evt event.Event) {
+	m.Called(evt)
+	return
+}
