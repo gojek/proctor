5a68f3e804372842a50021986ac75b45e1d0d79f
William Albertus Dembo <william.dembo@go-jek.com>
2019-09-18 15:27:46 +0700

    [Dembo] Refactor extract standard message to own file

--- plugins/slack-notification-plugin/slack/message/message.go
+++ plugins/slack-notification-plugin/slack/message/message.go
@@ -1,25 +1,5 @@
 package message
 
-import "encoding/json"
-
 type Message interface {
 	JSON() (string, error)
-}
-
-type standardMessage struct {
-	Text string `json:"text"`
-}
-
-func (messageObject *standardMessage) JSON() (string, error) {
-	byteMessage, err := json.Marshal(messageObject)
-	if err != nil {
-		return "", err
-	}
-	return string(byteMessage), nil
-}
-
-func NewStandardMessage(text string) Message {
-	return &standardMessage{
-		Text: text,
-	}
 }
---
+++ plugins/slack-notification-plugin/slack/message/standard.go
@@ -0,0 +1,21 @@
+package message
+
+import "encoding/json"
+
+type standardMessage struct {
+	Text string `json:"text"`
+}
+
+func (messageObject *standardMessage) JSON() (string, error) {
+	byteMessage, err := json.Marshal(messageObject)
+	if err != nil {
+		return "", err
+	}
+	return string(byteMessage), nil
+}
+
+func NewStandardMessage(text string) Message {
+	return &standardMessage{
+		Text: text,
+	}
+}
