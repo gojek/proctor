332f2c7042c5e2daf4483838f84e5ba5bf294ebd
William Albertus Dembo <william.dembo@go-jek.com>
2019-09-18 11:38:05 +0700

    [Dembo] Add slack client integration test

---
+++ plugins/slack-notification-plugin/slack/client_integration_test.go
@@ -0,0 +1,53 @@
+package slack
+
+import (
+	"os"
+	"testing"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+type integrationContext interface {
+	setUp(t *testing.T)
+	tearDown()
+	instance() *integrationTestContext
+}
+
+type integrationTestContext struct {
+	slackClient SlackClient
+	slackUrl    string
+}
+
+func (context *integrationTestContext) setUp(t *testing.T) {
+	value, available := os.LookupEnv("ENABLE_PLUGIN_INTEGRATION_TEST")
+	if available != true || value != "true" {
+		t.SkipNow()
+	}
+	client := resty.New()
+	context.slackClient = NewSlackClient(client)
+	slackUrl, _ := os.LookupEnv("SLACK_PLUGIN_URL")
+	assert.NotEmpty(t, slackUrl)
+	context.slackUrl = slackUrl
+}
+
+func (context *integrationTestContext) tearDown() {
+}
+
+func (context *integrationTestContext) instance() *integrationTestContext {
+	return context
+}
+
+func newIntegrationContext() integrationContext {
+	return &integrationTestContext{}
+}
+
+func TestSlackClientIntegration_Publish(t *testing.T) {
+	ctx := newIntegrationContext()
+	ctx.setUp(t)
+	defer ctx.tearDown()
+
+	message := NewStandardMessage("Message from slack plugin integration test with standard message")
+	err := ctx.instance().slackClient.Publish(message)
+	assert.NoError(t, err)
+}
