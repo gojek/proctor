4ad8418cf5c7c19895a000eeccc692a263f32d9e
Deny Prasetyo <jasoet87@gmail.com>
2019-12-16 11:34:23 +0700

    [Jasoet] fix(version): Client Version to v2.0.3

--- internal/app/cli/command/version/version.go
+++ internal/app/cli/command/version/version.go
@@ -9,7 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const ClientVersion = "v2.0.2"
+const ClientVersion = "v2.0.3"
 
 func NewCmd(printer io.Printer, fetcher github.LatestReleaseFetcher) *cobra.Command {
 	return &cobra.Command{
