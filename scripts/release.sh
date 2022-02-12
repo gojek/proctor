6688086237117e3c012d0a75a8d4e1187bd8fc90
Harshal Bhatia <harshalbhatia244@gmail.com>
2022-02-12 15:38:17 +0530

    Update release CI step with conditional tag logic

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,17 +2,16 @@
 
 set -e
 
-if [ -n "$TRAVIS_TAG" ]; then
-  curl -sL https://git.io/goreleaser | bash
-  SHA=$(cat dist/checksums.txt | grep Darwin_x86_64 | awk '{ print $1}')
-  go run scripts/proctor_template.go $TRAVIS_TAG $SHA
-  rm -rf homebrew-gojek
-  git clone "https://$GITHUB_TOKEN:@github.com/gojek/homebrew-gojek.git"
-  cp scripts/proctor.rb homebrew-gojek/Formula/proctor.rb
-  cd homebrew-gojek
-  git add .
-  git commit -m "[TravisCI] updating brew formula for release $TRAVIS_TAG"
-  git push --force --quiet "https://$GITHUB_TOKEN:@github.com/gojektech/homebrew-gojek.git"
-fi
-
+curl -sL https://git.io/goreleaser | bash
+SHA=$(cat dist/checksums.txt | grep Darwin_x86_64 | awk '{ print $1}')
+echo " REF_NAME => $GITHUB_REF_NAME"
+echo " REF => $GITHUB_REF"
+#go run scripts/proctor_template.go $GITHUB_REF_NAME $SHA
+#rm -rf homebrew-gojek
+#git clone "https://$GITHUB_TOKEN:@github.com/gojek/homebrew-gojek.git"
+#cp scripts/proctor.rb homebrew-gojek/Formula/proctor.rb
+#cd homebrew-gojek
+#git add .
+#git commit -m "[TravisCI] updating brew formula for release $TRAVIS_TAG"
+#git push --force --quiet "https://$GITHUB_TOKEN:@github.com/gojektech/homebrew-gojek.git"
 
