650d932b17da92ef08624b643dfabe9f5336f0a7
nikhilesh-parupalli <nikhilesh.reddy.99@gmail.com>
2022-03-24 13:38:37 +0530

    Update homebrew repo to new repository

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,11 +6,13 @@ curl -sL https://git.io/goreleaser | bash
 SHA=$(cat dist/checksums.txt | grep Darwin_x86_64 | awk '{ print $1}')
 echo " REF_NAME => $GITHUB_REF_NAME"
 go run scripts/proctor_template.go "$GITHUB_REF_NAME" "$SHA"
-rm -rf homebrew-gojek
-git clone "https://$GITHUB_TOKEN:@github.com/gojek/homebrew-gojek.git"
-cp scripts/proctor.rb homebrew-gojek/Formula/proctor.rb
-cd homebrew-gojek
+rm -rf homebrew-tap
+git clone "https://$GITHUB_TOKEN:@github.com/gojek/homebrew-tap.git"
+cp scripts/proctor.rb homebrew-tap/Formula/proctor.rb
+cd homebrew-tap
+git config user.name "Github Actions"
+git config user.email "<>"
 git add .
 git commit -m "[GH Actions] updating brew formula for release $GITHUB_REF_NAME"
-#git push --force --quiet "https://$GITHUB_TOKEN:@github.com/gojektech/homebrew-gojek.git"
+git push --force --quiet "https://$GITHUB_TOKEN:@github.com/gojektech/homebrew-tap.git"
 
